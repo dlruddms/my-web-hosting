d57dff8e34cd49f38ff323ded9d61a935f1d3885
dlruddms <141096632+dlruddms@users.noreply.github.com>
2023-08-08 17:28:33 +0900

    Add files via upload
    수

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const title = document.querySelector(".hello h1");
 
-title.innerText ="Hello";
+const score =parseInt(prompt("성적을 입력하세요"));
+
+if(isNaN(score) || score <0){
+	console.log("please write a real positive number");
+}
+else if (score>=90){
+ console.log ("A");
+}
+else if(score>=80 && score<90){
+	console.log("B");
+}
+else if(score>=70 && score <80){
+	console.log("C");
+}
+else (score <70)
+  {console.log("D")
+}
