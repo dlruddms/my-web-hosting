742c20ee2568b5011f1f213b14a8178fc97f08e0
dlruddms <141096632+dlruddms@users.noreply.github.com>
2023-08-11 01:10:42 +0900

    challenge 8

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const h1 = document.querySelector(".hello h1");
-let a=0;
+const h1 = document.querySelector("button");
 
-function handleTitleClick() {
-	a=a+1
-	h1.innerText = "click: " + a;
+function handTitleClick(){
+   h1.classList.toggle("clicked");
+   if(h1.innerText === "확인"){
+    h1.innerText = "취소"
+  }
+  else{ 
+    h1.innerText ="확인"
+  }
 }
 
-h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("click",handTitleClick)
